0329dec28ee50de482cb369bd009e9a059837271
zhanggh01 <88638513@qq.com>
2021-12-28 00:20:47 +0800

    123213213

---
+++ src/Test.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class Test {
+
+
+    public static void main(String[] args) {
+        String aa="LC|";
+
+        String aa1="010,011,012";
+        if(aa1.indexOf("010") >-1  && aa1.indexOf("011") >-1 ){
+            System.out.print("111111111");
+        }else{
+            System.out.print("444444="+aa1.indexOf("010"));
+        }
+
+        System.out.println(new Random().nextInt(10));
+
+    }
+    
+}
