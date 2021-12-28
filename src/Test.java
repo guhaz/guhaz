96b9d22182e11e9cef6f49c9c830991c44e3cccf
zhanggh01 <88638513@qq.com>
2021-12-28 16:53:21 +0800

    第十四次提交测试

--- src/Test.java
+++ src/Test.java
@@ -6,12 +6,12 @@ public class Test {
     public static void main(String[] args) {
         String aa="LC|";
 
-        String aa1="010,011,012.014,010";
+        String aa1="010,011,012.014,010，014";
         if(aa1.indexOf("010") >-1  && aa1.indexOf("011") >-1 ){
             System.out.print("111111111111");
         }else{
             System.out.print("444444="+aa1.indexOf("010"));
-        }
+    }
 
         System.out.println(new Random().nextInt(10));
 
