f5e05cdae28b6c2fbe422f99b384862e860bb8d9
zhanggh01 <88638513@qq.com>
2021-12-28 19:08:46 +0800

    Revert "第十五次提交测试130"
    This reverts commit 521ec251

--- src/Test.java
+++ src/Test.java
@@ -6,7 +6,7 @@ public class Test {
     public static void main(String[] args) {
         String aa="LC|";
 
-        String aa1="010,011,012.014,010，014，0130";
+        String aa1="010,011,012.014,010，014，0155";
         if(aa1.indexOf("010") >-1  && aa1.indexOf("011") >-1 ){
             System.out.print("111111111111");
         }else{
