d5e30287c5aaf407af33cea4aca0304404161a08
zhanggh01 <88638513@qq.com>
2021-12-29 23:43:04 +0800

    5

--- src/Test.java
+++ src/Test.java
@@ -9,12 +9,13 @@ public class Test {
         String aa1="010,011,012.014,010，014，0155";
         String aa2="010,011,012.014,010，014，0155";
         String aa34="010,011,012.014,010，014，0155";
+        String aa36="010,011,012.014,010，014，0155";
         if(aa1.indexOf("010") >-1  && aa1.indexOf("011") >-1 ){
             System.out.print("111111111111");
         }else{
             System.out.print("444444="+aa1.indexOf("010"));
     }
-
+        
         System.out.println(new Random().nextInt(10));
 
     }
